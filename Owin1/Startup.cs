d17dd94dab1d6668583a65c00bec3824d34c228b
reagul <reagul.2007@gmail.com>
2017-03-31 13:12:37 -1200

    first commit

---
+++ Owin1/Startup.cs
@@ -0,0 +1,30 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace Owin1
+{
+    public class Startup
+    {
+        public static void Configuration(IAppBuilder app)
+        {
+            app.Use(async (ctx , next) => { 
+
+                Debug.WriteLine("Incoming request" + ctx.Request.Path);
+                await next();
+                Debug.WriteLine("###Outgoing response" + ctx.Request.Path);
+            });
+
+            app.Use(async (ctx , next) => {
+
+                await ctx.Response.WriteAsync("From OWIN Katana - Welcome to PCF");
+
+            });
+
+
+        }
+    }
+}
